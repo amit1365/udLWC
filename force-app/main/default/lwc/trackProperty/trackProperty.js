9ed7f249d8459283aad2486a8ee37ef668d95f61
amit1365 <amityadavlooks@gmail.com>
2025-06-15 19:09:13 +0530

    Added Getters code

--- force-app/main/default/lwc/trackProperty/trackProperty.js
+++ force-app/main/default/lwc/trackProperty/trackProperty.js
@@ -2,6 +2,9 @@ import { LightningElement, track} from 'lwc';
 
 export default class TrackProperty extends LightningElement {
 
+    number1 = 0;
+    number2 = 0;
+
     //track decorator is used to make a property reactive
     //when the property value changes, the component re-renders, it is used to track the changes in the property of array or object
     
@@ -26,5 +29,21 @@ export default class TrackProperty extends LightningElement {
 
     callHandler4(event){
         this.addressObj = {...this.addressObj, "pin":event.target.value};
+    }
+
+    //getters and setters are used to define a property in a class
+    
+    get summ() {
+        //alert('getters are called');
+        return (this.number1 + this.number2);
+        
+    }
+
+    num1change(event){
+        this.number1 = parseInt(event.target.value,10);
+    }
+
+    num2change(event){
+        this.number2 = parseInt(event.target.value,10);
     }
 }
